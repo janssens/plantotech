cae7d383e915ff0d0eac7a29f02521cbc88541ba
Gaetan Janssens <janssensgaetan@gmail.com>
2020-11-19 08:22:53 +0100

    make slug static function of Plant

--- src/Command/ImportCommand.php
+++ src/Command/ImportCommand.php
@@ -1009,7 +1009,7 @@ protected static $defaultName = 'app:import-from-db';
         if (!$code){
             $code = $label;
         }
-        $code = self::makeSlug($code);
+        $code = Plant::makeSlug($code);
         if (!$code || strlen($code)===0){
             $code = 'none';
         }
@@ -1067,24 +1067,5 @@ protected static $defaultName = 'app:import-from-db';
         return $exist;
     }
 
-    static function makeSlug($string,$length = 50){
-        //Lower case everything
-        $string = strtolower($string);
-        //remove accent
-        $unwanted_array = array(    'Š'=>'S', 'š'=>'s', 'Ž'=>'Z', 'ž'=>'z', 'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A', 'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C', 'È'=>'E', 'É'=>'E',
-            'Ê'=>'E', 'Ë'=>'E', 'Ì'=>'I', 'Í'=>'I', 'Î'=>'I', 'Ï'=>'I', 'Ñ'=>'N', 'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O', 'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ù'=>'U',
-            'Ú'=>'U', 'Û'=>'U', 'Ü'=>'U', 'Ý'=>'Y', 'Þ'=>'B', 'ß'=>'Ss', 'à'=>'a', 'á'=>'a', 'â'=>'a', 'ã'=>'a', 'ä'=>'a', 'å'=>'a', 'æ'=>'a', 'ç'=>'c',
-            'è'=>'e', 'é'=>'e', 'ê'=>'e', 'ë'=>'e', 'ì'=>'i', 'í'=>'i', 'î'=>'i', 'ï'=>'i', 'ð'=>'o', 'ñ'=>'n', 'ò'=>'o', 'ó'=>'o', 'ô'=>'o', 'õ'=>'o',
-            'ö'=>'o', 'ø'=>'o', 'ù'=>'u', 'ú'=>'u', 'û'=>'u', 'ý'=>'y', 'þ'=>'b', 'ÿ'=>'y' );
-        $string = strtr( $string, $unwanted_array );
-        //Make alphanumeric (removes all other characters)
-        $string = preg_replace("/[^a-z0-9+_\s-]/", "", $string);
-        //Clean up multiple dashes or whitespaces
-        $string = preg_replace("/[\s-]+/", " ", $string);
-        //Convert whitespaces and underscore to dash
-        $string = preg_replace("/[\s_]/", "-", $string);
-        //shorten
-        $string = substr($string,0,$length);
-        return $string;
-    }
+
 }
--- src/Entity/Plant.php
+++ src/Entity/Plant.php
@@ -304,11 +304,28 @@ class Plant
 
     public function getSlug(): string
     {
-        return strtolower(trim(preg_replace('/[\s-]+/', '-',
-            preg_replace('/[^A-Za-z0-9-]+/', '-',
-                preg_replace('/[&]/', 'and',
-                    preg_replace('/[\']/', '',
-                        iconv('UTF-8', 'ASCII//TRANSLIT', $this->getName()))))), '-'));
+        return self::makeSlug($this->getLatinName());
+    }
+
+    static function makeSlug($string,$length = 50){
+        //Lower case everything
+        $string = strtolower($string);
+        //remove accent
+        $unwanted_array = array(    'Š'=>'S', 'š'=>'s', 'Ž'=>'Z', 'ž'=>'z', 'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A', 'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C', 'È'=>'E', 'É'=>'E',
+            'Ê'=>'E', 'Ë'=>'E', 'Ì'=>'I', 'Í'=>'I', 'Î'=>'I', 'Ï'=>'I', 'Ñ'=>'N', 'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O', 'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ù'=>'U',
+            'Ú'=>'U', 'Û'=>'U', 'Ü'=>'U', 'Ý'=>'Y', 'Þ'=>'B', 'ß'=>'Ss', 'à'=>'a', 'á'=>'a', 'â'=>'a', 'ã'=>'a', 'ä'=>'a', 'å'=>'a', 'æ'=>'a', 'ç'=>'c',
+            'è'=>'e', 'é'=>'e', 'ê'=>'e', 'ë'=>'e', 'ì'=>'i', 'í'=>'i', 'î'=>'i', 'ï'=>'i', 'ð'=>'o', 'ñ'=>'n', 'ò'=>'o', 'ó'=>'o', 'ô'=>'o', 'õ'=>'o',
+            'ö'=>'o', 'ø'=>'o', 'ù'=>'u', 'ú'=>'u', 'û'=>'u', 'ý'=>'y', 'þ'=>'b', 'ÿ'=>'y' );
+        $string = strtr( $string, $unwanted_array );
+        //Make alphanumeric (removes all other characters)
+        $string = preg_replace("/[^a-z0-9+_\s-]/", "", $string);
+        //Clean up multiple dashes or whitespaces
+        $string = preg_replace("/[\s-]+/", " ", $string);
+        //Convert whitespaces and underscore to dash
+        $string = preg_replace("/[\s_]/", "-", $string);
+        //shorten
+        $string = substr($string,0,$length);
+        return $string;
     }
 
     public function getLifeCycle(): ?int
