75161ead2960d976333645b71a9de7354d1d7802
Gaetan Janssens <janssensgaetan@gmail.com>
2021-03-19 10:21:46 +0100

    refactor mailer

--- src/Service/MailService.php
+++ src/Service/MailService.php
@@ -9,13 +9,33 @@ use Symfony\Component\Mailer\MailerInterface;
 Class MailService{
 
     private $mailer;
+    private $configService;
 
-    public function __construct(MailerInterface $mailer)
+    public function __construct(
+        MailerInterface $mailer,
+        ConfigService $configService
+    )
     {
         $this->mailer = $mailer;
+        $this->configService = $configService;
     }
 
-    public function sendMail(TemplatedEmail $email,int $count = 0){
+    public function sendMail(string $email,string $subject,string $template = 'default',array $context = [],$reply_to = null){
+        $emailTemplate = (new TemplatedEmail())
+            ->from($this->configService->getValue('app/main_email'))
+            ->to($email)
+            ->subject('['.$this->configService->getValue('app/website_name').'] '.$subject)
+            ->htmlTemplate('emails/'.$template.'.html.twig')
+            ->textTemplate('emails/'.$template.'.txt.twig')
+            ->context($context);
+        if ($reply_to && $reply_to != $email){
+            $emailTemplate->replyTo($reply_to);
+        }
+        return $this->send($emailTemplate,0);
+    }
+
+    private function send(TemplatedEmail $email,int $count = 0)
+    {
         try{
             $this->mailer->send($email);
             return 0;
@@ -24,7 +44,7 @@ Class MailService{
                 return $e;
             }
             // retry
-            return $this->sendMail($email,$count++);
+            return $this->send($email,$count++);
         }
     }
 }
