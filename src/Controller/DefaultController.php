435358c469862bf114db8b61f7d313ed3403a3b5
Gaetan Janssens <janssensgaetan@gmail.com>
2021-03-19 10:21:16 +0100

    refactor mailer

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -44,7 +44,6 @@ class DefaultController extends AbstractController
      */
     public function index(Request $request)
     {
-
         return $this->render('home/index.html.twig');
     }
 
@@ -75,37 +74,22 @@ class DefaultController extends AbstractController
                 $session->getFlashBag()->add('error', 'Un message à faire passer ? 📜');
                 return $this->render('home/contact.html.twig',array('name'=>$name,'subject'=>$subject,'email'=>$email_address,'message'=>$message));
             }
-            $email = (new TemplatedEmail())
-                ->from($this->config->getValue('app/main_email'))
-                ->to($email_address)
-                ->subject('['.$this->config->getValue('app/website_name').'] ton message a bien été transmit')
-                ->htmlTemplate('emails/contact_from.html.twig')
-                ->textTemplate('emails/contact_from.txt.twig')
-                ->context([
-                    'name' => $name,
-                    'mail' => $email_address,
-                    'subject' => $subject,
-                    'message' => $message,
-                ]);
-            if ($e = $this->mailer->sendMail($email)){
+            if ($e = $this->mailer->sendMail($email_address,'ton message a bien été transmit','contact_form',[
+                'name' => $name,
+                'mail' => $email_address,
+                'subject' => $subject,
+                'message' => $message,
+            ])){
                 $session->getFlashBag()->add('error', 'oups, l\'email de confirmation na pas pu être envoyé.'.
                     ' On réessaie ensemble dans un moment ?');
                 $session->getFlashBag()->add('warning', $e->getMessage());
             }else{
-                $email = (new TemplatedEmail())
-                    ->from($this->config->getValue('app/main_email'))
-                    ->to($this->config->getValue('app/main_email'))
-                    ->replyTo($email_address)
-                    ->subject('['.$this->config->getValue('app/website_name').'] contact : '.$subject)
-                    ->htmlTemplate('emails/contact_to.html.twig')
-                    ->textTemplate('emails/contact_to.txt.twig')
-                    ->context([
-                        'name' => $name,
-                        'mail' => $email_address,
-                        'subject' => $subject,
-                        'message' => $message,
-                    ]);
-                if ($e = $this->mailer->sendMail($email)){
+                if ($e = $this->mailer->sendMail($this->config->getValue('app/main_email'),'contact : '.$subject,'contact_to',[
+                    'name' => $name,
+                    'mail' => $email_address,
+                    'subject' => $subject,
+                    'message' => $message,
+                ],$email_address)){
                     $session->getFlashBag()->add('error', 'oups, l\'email de confirmation na pas pu être envoyé.'.
                         ' On réessaie ensemble dans un moment ?');
                     $session->getFlashBag()->add('warning', $e->getMessage());
